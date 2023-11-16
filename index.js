9245c124aa0a44be327cb9ecfd4498c16e262e7e
Aruna.jayathilaka <Aruna.jayathilaka@austal.com>
2023-11-17 07:26:55 +0800

    updated

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const app = express();
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
 
-var serviceAcc = {
-  "type": process.env.type,
-  "project_id": process.env.project_id,
-  "private_key_id": process.env.private_key_id,
-  "private_key": process.env.private_key,
-  "client_email": process.env.client_email,
-  "client_id": process.env.client_id,
-  "auth_uri":  process.env.auth_uri,
-  "token_uri":  process.env.token_uri,
-  "auth_provider_x509_cert_url":  process.env.auth_provider_x509_cert_url,
-  "client_x509_cert_url":  process.env.client_x509_cert_url,
-  "universe_domain": process.env.universe_domain,
-}
+// var serviceAcc = {
+//   "type": process.env.type,
+//   "project_id": process.env.project_id,
+//   "private_key_id": process.env.private_key_id,
+//   "private_key": process.env.private_key,
+//   "client_email": process.env.client_email,
+//   "client_id": process.env.client_id,
+//   "auth_uri":  process.env.auth_uri,
+//   "token_uri":  process.env.token_uri,
+//   "auth_provider_x509_cert_url":  process.env.auth_provider_x509_cert_url,
+//   "client_x509_cert_url":  process.env.client_x509_cert_url,
+//   "universe_domain": process.env.universe_domain,
+// }
 
 initializeApp({
-  credential: cert(serviceAcc),
+  credential: cert(process.env.serviceAcc),
   databaseURL: process.env.databaseURL,
 });
 //Sets our app to use the handlebars engine

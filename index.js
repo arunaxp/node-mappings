5a0be66aac3b06b89c50eaaeca36e8cce29ff0ee
Aruna.jayathilaka <Aruna.jayathilaka@austal.com>
2023-11-16 14:24:54 +0800

    updated:website

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAcc = require('./ferrous-store-service-account.json');
 const express = require('express');
 const app = express();
-const port = 8080;
 
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
 
+initializeApp({
+  credential: cert(serviceAcc),
+  databaseURL: 'ferrous-store-404700.firebaseio.com', //process.env.databaseURL,
+});
 //Sets our app to use the handlebars engine
 app.set('view engine', 'hbs');
 
+// var hbs = handlebars.create({});
+// hbs.handlebars.registerHelper("increasePrice", function (price) {
+//   price += 10;
+//   return price;
+// });
 //Sets handlebars configurations (we will go through them later on)
 app.engine(
   'hbs',
@@ -21,16 +32,53 @@ app.engine(
 
 app.use('/public', express.static('public'));
 
-app.get('/', (req, res) => {
-  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-  res.render('main', {layout: 'index'});
+const alldata = async () => {
+  const db = getFirestore();
+  var d = [];
+  const snapshot = await db.collection('SampleDocs').get();
+  snapshot.forEach((doc) => {
+    //console.log(doc.data()?.Name);
+    d.push({ name: doc.data()?.Name });
+  });
+  console.log(d);
+  return d;
+};
+
+app.get('/', async (req, res) => {
+  res.render('main', { layout: 'index', data: await alldata() });
 });
 
 const fakeApi = () => 'Faker';
 
-app.get('/dynamic', (req, res) => {
-  res.render('main', {layout: 'index', proPlayer: fakeApi()});
+app.get('/dynamic2', (req, res) => {
+  res.render('main', { layout: 'index', proPlayer: fakeApi() });
 });
+
+// app.get('/testDB', async (req, res) => {
+//   const db = getFirestore();
+
+//   const snapshot = await db.collection('SampleDocs').get();
+
+//   // Print the ID and contents of each document
+//   var d = '';
+//   snapshot.forEach((doc) => {
+//     console.log(doc.id, ' => ', doc.data());
+//     d += doc.data()?.Name;
+//   });
+//   res.send({ Message: d });
+// });
+
+// Handlebars.registerHelper('SampleDocs', async function () {
+//   const db = getFirestore();
+//   var d = ['A', 'B'];
+//   const snapshot = await db.collection('SampleDocs').get();
+//   snapshot.forEach((doc) => {
+//     console.log(doc.data()?.Name);
+//     d.push(doc.data()?.Name);
+//   });
+//   console.log(d);
+//   return d; //just return global variable value
+// });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
